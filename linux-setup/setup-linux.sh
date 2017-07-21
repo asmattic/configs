b24f92ea933f4b462853972c8af838d2c3819b07
Asmattic <mattoldfieldweb@gmail.com>
2017-07-21 03:56:40 +0000

    vim setup files created successfully

--- linux-setup/setup-linux.sh
+++ linux-setup/setup-linux.sh
@@ -6,39 +6,44 @@
 # Date: 07/20/2017
 # Created By: Matthew Oldfield
 
+# General variables
+base_dir=$(pwd)
+home_dir="/home/pi"
+
 # Create vim style directories (mkdir -p creates all dir levels)
-vim_dir='~/.vim'
-vim_colors_dir='~/.vim/colors'
-vim_monokai_file='./files/monokai.vim'
+vim_dir="$home_dir/.vim"
+vim_colors_dir="$home_dir/.vim/colors"
+vim_monokai_file="$base_dir/files/monokai.vim"
 
 # Check if dirs exist, create if needed
-if [ ! -d '$vim_dir']
+if [ ! -d $vim_dir ]
 then
-	echo '$vim_dir does not exist, create $vim_colors_dir'
+	echo $vim_dir does not exist, create $vim_colors_dir
 	mkdir -p $vim_colors_dir
-elif [ ! -d '$vim_colors_dir']
+elif [ ! -d $vim_colors_dir ]
 then
-	echo '$vim_colors_dir does not exist, create it'
+	echo $vim_colors_dir does not exist, create it
 	mkdir -p $vim_colors_dir
 else
-	echo '$vim_dir and $vim_colors_dir exist'
-	echo 'copy $vim_monokai_file to $vim_colors_dir'
+	echo $vim_dir and $vim_colors_dir exist
+	echo copy $vim_monokai_file to $vim_colors_dir
 fi
 
 # Place monokai.vim in colors dir
+
 cp $vim_monokai_file $vim_colors_dir
 
 # Check if .vimrc file exists, then write to it or create
-vimrc_file='~/.vimrc'
-if [ -w '$vimrc_file']
+vimrc_file="$home_dir/.vimrc"
+if [ -w $vimrc_file ]
 then
-	echo '$vimrc_file exists, appending monokai settings'
+	echo $vimrc_file exists, appending monokai settings
 else
-	echo '$vimrc_file does not exist, create it'
+	echo $vimrc_file does not exist, create it
 	touch $vimrc_file
 fi
 
 # Append monokai settings
-echo 'Appending monokai settings'
-echo 'syntax enable' | sudo tee -a $vimrc_file
-echo 'colorscheme monokai' | sudo tee -a $vimrc_file
+echo Appending monokai settings
+echo "syntax enable" | sudo tee -a $vimrc_file
+echo "colorscheme monokai" | sudo tee -a $vimrc_file
